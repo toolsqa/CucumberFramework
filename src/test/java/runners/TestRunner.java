191519f3c195c6db276d0e9256ed90f0628a4270
toolsqa <lakshay@toolsqa.com>
2018-01-20 19:56:14 +0100

    Chapter-13

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,14 +1,17 @@
 package runners;
 
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/functionalTests",
-		glue= {"stepDefinitions"}
+		glue= {"stepDefinitions"},
+		plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
+				"junit:target/cucumber-reports/Cucumber.xml",
+				"html:target/cucumber-reports"},
+		monochrome = true
 		)
  
 
